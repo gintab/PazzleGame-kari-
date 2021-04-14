20c3ebb33bf6453711de39db90b921ec033496bd
Color0525 <rio.game.kojima@gmail.com>
2021-04-14 12:18:48 +0900

    player追加

---
+++ PazzleGamekari/Assets/Kojima/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 5f;
+    bool m_moveing = false;
+    Rigidbody2D m_rb;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 dir = Vector2.zero;
+
+        if (dir != Vector2.zero)
+        {
+            m_rb.velocity = dir * m_speed;
+        }
+
+        if (!m_moveing)
+        {
+            float v = Input.GetAxisRaw("Vertical");
+            float h = Input.GetAxisRaw("Horizontal");
+
+            if (v != 0)
+            {
+                dir.y = v;
+            }
+            else if (h != 0)
+            {
+                dir.x = h;
+            }
+            Debug.Log(dir);
+        }
+    }
+}
