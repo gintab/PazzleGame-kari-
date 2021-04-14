9fc6df7efac6c082fec04400dd59e0533a269f6e
DESKTOP-FTQ7RG4\vantan <katoon.katudon@icloud.com>
2021-04-14 13:34:42 +0900

    2回目

--- PazzleGamekari/Assets/Takahashi/ChangeBlock.cs
+++ PazzleGamekari/Assets/Takahashi/ChangeBlock.cs
@@ -2,27 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wall : MonoBehaviour
+public class ChangeBlock : MonoBehaviour
 {
-    GameObject player;
+    GameObject block;
     Rigidbody2D m_rb;
     // Start is called before the first frame update
     void Start()
     {
-        m_rb = GetComponent<Rigidbody2D>(); 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        if (Input.GetKeyDown("w")|| Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
         {
-            Debug.Log("hit");
+            
         }
     }
 }
