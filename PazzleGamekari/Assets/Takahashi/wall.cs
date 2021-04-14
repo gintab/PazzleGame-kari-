48d5649e3d819233ac019771dd238d6b13e58751
DESKTOP-FTQ7RG4\vantan <katoon.katudon@icloud.com>
2021-04-14 12:21:23 +0900

    仮当たり判定

---
+++ PazzleGamekari/Assets/Takahashi/wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody2D m_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit");
+        }
+    }
+}
