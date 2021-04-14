658651aa0292dbabe88d959fadeb103f04e2649b
gintab <ginkana1207@gmail.com>
2021-04-14 17:13:27 +0900

    AudioManagerと背景画像

---
+++ PazzleGamekari/Assets/yamaychi/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+    // Start is called before the first frame update
+    AudioSource audioSource = null;
+    [SerializeField] AudioClip Suberi = null;
+    [SerializeField] AudioClip Stop = null;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void SubeSE()
+    {
+        audioSource.PlayOneShot(Suberi);
+    }
+    public void StopSE()
+    {
+        audioSource.PlayOneShot(Stop);
+    }
+}
