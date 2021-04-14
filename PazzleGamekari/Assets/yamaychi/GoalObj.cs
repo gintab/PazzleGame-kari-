255dcbc9b09bef291e5d0dde91ed6337b783e3fd
gintab <ginkana1207@gmail.com>
2021-04-14 14:38:33 +0900

    直接

---
+++ PazzleGamekari/Assets/yamaychi/GoalObj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalObj : Block
+{
+    GameObject canvas = null;
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+    public override void Hit()
+    {
+        Instantiate(Resources.Load("ClearPanel"), canvas.transform);
+    }
+}
