f92d75c0584fbc39935c4a0607aefd61e81754b8
Color0525 <rio.game.kojima@gmail.com>
2021-04-14 16:08:46 +0900

    player未完成

--- PazzleGamekari/Assets/Kojima/PlayerController.cs
+++ PazzleGamekari/Assets/Kojima/PlayerController.cs
@@ -6,77 +6,79 @@ using DG.Tweening;
 
 public class PlayerController : SingletonMonoBehaviour<PlayerController>
 {
-    [SerializeField] float m_boxSize = 50f;
-    [SerializeField] float m_oneBoxSecond = 5f;
-    [SerializeField] GameObject m_HitCollider = null;
+    [SerializeField] float m_squareSize = 50f;
+    [SerializeField] float m_oneSquareSecond = 1f;
     [SerializeField] LayerMask m_blockLayer;
 
-
     Vector2 m_dir = Vector2.zero;
+    
     bool m_moveing = false;
-    //Rigidbody2D m_rb;
 
     void Start()
     {
-        //m_rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if (!m_moveing)
         {
-            float v = Input.GetAxisRaw("Vertical");
-            float h = Input.GetAxisRaw("Horizontal");
-
-            if (v != 0)
+            if (Input.GetButtonDown("Vertical") || Input.GetButtonDown("Horizontal"))
             {
-                m_moveing = true;
-                m_dir = new Vector2(0, v);
-            }
-            else if (h != 0)
-            {
-                m_moveing = true;
-                m_dir = new Vector2(h, 0);
-            }
-        }
+                float v = Input.GetAxisRaw("Vertical");
+                float h = Input.GetAxisRaw("Horizontal");
 
-        if (m_moveing)
-        {
-            Move();
-            //m_rb.velocity = m_dir * m_speed;
+                if (v != 0)
+                {
+                    m_moveing = true;
+                    m_dir = new Vector2(0, v);
+                    StartCoroutine(Move());
+                }
+                else if (h != 0)
+                {
+                    m_moveing = true;
+                    m_dir = new Vector2(h, 0);
+                    StartCoroutine(Move());
+                }
+            }
         }
     }
 
     public void Stop()
     {
         m_moveing = false;
-        //m_rb.velocity = Vector2.zero;
     }
 
-    //void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Stop();
-    //}
-
-    void Move()
+    IEnumerator Move()
     {
-        Vector2 movePos = (Vector2)this.transform.position + m_dir * m_boxSize;
-
-        Debug.DrawLine(this.transform.position, movePos);
-        RaycastHit2D hitBlock = Physics2D.Raycast(this.transform.position, m_dir, m_boxSize, m_blockLayer);
-        if (hitBlock.collider)
+        //bool m = false;
+        while (m_moveing)
         {
-            hitBlock.collider.gameObject.GetComponent<Block>().Hit();
+            Vector2 movePos = (Vector2)this.transform.position + m_dir * m_squareSize;
+            Debug.Log(m_dir);
+
+            RaycastHit2D hitBlock = Physics2D.Raycast(this.transform.position, m_dir, m_squareSize, m_blockLayer);
+            Debug.DrawLine(this.transform.position, (Vector2)this.transform.position + m_dir * m_squareSize);
+
+            if (hitBlock.collider)
+            {
+                hitBlock.collider.gameObject.GetComponent<Block>().Hit();
+                m_moveing = false;
+            }
+            else
+            {
+                Debug.Log(0);
+                yield return StartCoroutine(DM(movePos));//transform.DOMove(movePos, m_oneSquareSecond).SetEase(Ease.Linear);
+            }
+
+            //m_moveing = false;
+
         }
-        else
+
+        IEnumerator DM(Vector2 movePos)
         {
-            transform.DOMove(movePos, m_oneBoxSecond);
-
+            transform.DOMove(movePos, m_oneSquareSecond);//.SetEase(Ease.Linear);
+            yield return null;
         }
-
-        
-
-        
     } 
 }
 
